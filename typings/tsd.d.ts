2a97406daced617d7d0f80c71ea86f697dd72fa4
Josh Bender <jbendercode@gmail.com>
2016-02-08 22:44:30 +0000

    Added-gulp-and-tasks

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,7 @@
 /// <reference path="dat-gui/dat-gui.d.ts" />
 /// <reference path="../Scripts/objects/control.ts" />
 /// <reference path="stats/stats.d.ts" />
+/// <reference path="gulp/gulp.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="orchestrator/orchestrator.d.ts" />
+/// <reference path="q/Q.d.ts" />
